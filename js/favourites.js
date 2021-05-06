ebfc680a7abec73d83d4302e1650e5b560b2e1d4
unknown <dalia.sireikyte@gmail.com>
2021-05-06 14:58:49 +0200

    Fixed ClearAll button

--- js/favourites.js
+++ js/favourites.js
@@ -1,36 +1,38 @@
 import { articlesContainer } from "./components/targetElements.js";
 import { getFavArticles } from "./utils/storage.js";
-// import { displayMessage } from "./utils/displayMessage.js";
-// import { noFavs } from "./components/messages.js";
-import { clearButton } from "./utils/clearAll.js";
+import { displayMessage } from "./utils/displayMessage.js";
+import { noFavs } from "./components/messages.js";
+import { clearList } from "./utils/clearAll.js";
 
-const clearAllButton = document.querySelector(".clearButton");
-const favArticles = getFavArticles();
-clearButton(favArticles);
-
-export function renderFavs() {
+function renderFavs() {
+    const clearAllButton = document.querySelector(".clearButton");
     articlesContainer.innerHTML = "";
 
-    // if (favArticles.length === 0) {
-    //     displayMessage("", noFavs, ".articles-container");
-    //     clearAllButton.style.display = 'none';
-    // }
-
-    favArticles.forEach((article) => {
-        articlesContainer.innerHTML += `
-            <div class="col p-4">
-                <div class="card">
-                    <div class="card-body article">
-                        <h5 class="card-title">${article.title}</h5>
-                        <p class="card-text">${article.summary}</p>
-                        <p class="card-footer bg-transparent fst-italic m-0">${article.author}</p>
-                        <i class="fa fa-heart"></i>
-                    </div>
-                </div>
-            </div>
-            `;
+    clearAllButton.addEventListener("click", () => {
+        clearList(renderFavs);
     });
 
+    const favArticles = getFavArticles();
+
+    if (favArticles.length === 0) {
+        displayMessage("", noFavs, ".articles-container");
+        clearAllButton.style.display = 'none';
+    } else {
+        favArticles.forEach((article) => {
+            articlesContainer.innerHTML += `
+                <div class="col p-4">
+                    <div class="card">
+                        <div class="card-body article">
+                            <h5 class="card-title">${article.title}</h5>
+                            <p class="card-text">${article.summary}</p>
+                            <p class="card-footer bg-transparent fst-italic m-0">${article.author}</p>
+                            <i class="fa fa-heart"></i>
+                        </div>
+                    </div>
+                </div>
+                `;
+        });
+    }
 }
 
 renderFavs();
--- js/utils/clearAll.js
+++ js/utils/clearAll.js
@@ -1,21 +1,6 @@
-import { getFavArticles } from "./storage.js";
-import { displayMessage } from "./displayMessage.js";
-import { noFavs } from "../components/messages.js";
-
-export function clearButton() {
-    const clearAllButton = document.querySelector(".clearButton");
-    clearAllButton.addEventListener("click", clearList);
-
-    function clearList() {
-        if (confirm(`Are you sure you want to remove all your favourites?`)) {
-            localStorage.clear();
-        }
-        const favArticles = getFavArticles();
-        console.log(favArticles);
-        if (favArticles.length === 0) {
-            displayMessage("", noFavs, ".articles-container");
-            clearAllButton.style.display = 'none';
-        }
+export function clearList(renderFavs) {
+    if (confirm(`Are you sure you want to remove all your favourites?`)) {
+        localStorage.clear();
+        renderFavs();
     }
-
 }
